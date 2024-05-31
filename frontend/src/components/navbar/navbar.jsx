9425cd3c54b3a9443164b3acd037f4a555f9a028
Divyansh Gupta <123203281+divyansh12git@users.noreply.github.com>
2024-05-31 22:48:25 +0530

    hero section completed

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import DescoLogo from "../../assets/images/Desco_logo.webp"
 import { Link } from 'react-router-dom'
 const NavBar=()=>{
-    return <nav className='h-16 mx-10  bg-[#FE3D4D] rounded-xl px-10 grid grid-cols-3 items-center'>
+    return <nav className='z-20 h-16 mx-10 absolute top-6  bg-[#FE3D4D] rounded-xl px-10 grid grid-cols-3 items-center'>
         <div className='w-56 h-auto flex items-center gap-5 justify-self-start'>
             <img src={DescoLogo} alt="Desco" className='h-9' />
             <p className='text-white text-xl'>Design Company</p>
