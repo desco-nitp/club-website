2cc778b5707ddd97a66f263a47fb47030811a212
Divyansh Gupta <123203281+divyansh12git@users.noreply.github.com>
2024-06-14 01:41:13 +0530

    feature and category component completed (#37)
    * ci pipeline updated
    
    * syntax err fixed in ci pipeline
    
    * ci test 1
    
    * test 2
    
    * test 3
    
    * test 4
    
    * test5
    
    * test6
    
    * test 7
    
    * Update view.yaml
    
    * pipeline updated for only pull
    
    * ci update 2
    
    * ci5
    
    * removing vercel
    
    * mobile view navbar completed
    
    * blog part1 components featue/category

---
+++ frontend/src/components/blogs/part1.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Feature from './feature/featuredpost'
+import Categories from './feature/categories'
+
+function Part1() {
+  return (
+    <div className='bg-[#FEB5B5] mt-10 mb-20 lg:grid  md:grid-cols-2  '>
+        <div className='flex justify-center' >
+            <Feature />
+        </div>
+        <div className='flex justify-center items-center  h-[110vh]'>
+            <Categories />
+        </div>
+    
+    </div>
+    
+  )
+}
+
+export default Part1
