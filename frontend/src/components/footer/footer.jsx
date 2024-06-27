f794e4022fdc8a6f2e39415081e03dd7907a94eb
Ansh Gusain <119176377+007ANSH@users.noreply.github.com>
2024-06-27 15:16:05 +0530

    footer update.  (#45)
    * overlap resolve
    
    * Created Hero component of Team Page
    
    * latest post in blogs done
    
    * fixed warnings
    
    * oval footer fixed

--- frontend/src/components/footer/footer.jsx
+++ frontend/src/components/footer/footer.jsx
@@ -9,9 +9,9 @@ const Footer = () => {
         <div className='pl-5 w-[9rem] pr-5 text-xs lg:w-[20rem] sm:w-[25rem] sm:text-[0.9rem]'><span>&copy; 2023 DesignCompany. All rights reserved</span></div>
         <div className='sm:w-[20rem] text-xs px-1 mx-2 sm:text-xl h-8 sm:h-10 flex'><img src={DescoLogo} alt="" /> <span className='mx-2 sm:mx-0 sm:p-1'>Design Company</span></div>
         <div className=' flex  '>
-            <div><img className=' sm:h-14 h-[2.5rem] p-1 sm:mx-3 mx-1' src={Facebook} alt="" /> </div>
-            <div><img className=' sm:h-14 h-[2.5rem] p-1 sm:mx-3 mx-1' onClick={() => window.location.href = "https://www.linkedin.com/company/desco-nitp/mycompany/"} src={Linkedin} alt="" /></div>
-            <div> <img className=' sm:h-14 h-[2.5rem] p-1 sm:mx-3 mx-1' onClick={()=>{window.location.href="https://www.instagram.com/desco.nitp/"}} src={Instagram} alt="" /> </div>
+            <div><img className=' sm:h-14 h-[2.3rem] p-1 sm:mx-3 mx-1' src={Facebook} alt="" /> </div>
+            <div><img className=' sm:h-14 h-[2.3rem] p-1 sm:mx-3 mx-1' onClick={() => window.location.href = "https://www.linkedin.com/company/desco-nitp/mycompany/"} src={Linkedin} alt="" /></div>
+            <div> <img className=' sm:h-14 h-[2.3rem] p-1 sm:mx-3 mx-1' onClick={()=>{window.location.href="https://www.instagram.com/desco.nitp/"}} src={Instagram} alt="" /> </div>
         </div>
     </div>
   )
