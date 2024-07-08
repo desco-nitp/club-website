7d09d73655305fc8f05ea3daa04c95d41cffb871
maithilee <pardhia.ug22.ec@nitp.ac.in>
2024-07-08 10:20:09 +0530

    updated

--- frontend/src/components/works/works_herosection/works_hero.jsx
+++ frontend/src/components/works/works_herosection/works_hero.jsx
@@ -14,8 +14,8 @@ import "./style.css"
 const WorkHero = () => {
     return (
         <div className="flex flex-col">
-            <div className="flex flex-row relative -top-[1.5rem] ">
-                <div className="w-1/3 relative">
+            <div className="flex flex-row relative -top-[5rem] md:-top-[1.5rem] ">
+                <div className=" hidden md:block w-1/3 relative">
                     <div className="absolute left-4 sm:left-8 lg:left-10 top-[5.5rem] sm:top-[6.5rem] xl:top-[8rem] z-20">
                         <img src={Star} alt="Star" className="h-[2rem] sm:h-[3.5rem] lg:h-[4rem] xl:h-[6rem]" />
                     </div>
@@ -29,8 +29,8 @@ const WorkHero = () => {
                         <img src={V4} alt="Vector" className="h-[3rem] sm:h-[5rem] lg:h-[8rem] xl:h-[11rem]"/>
                     </div>
                 </div>
-                <div className="w-1/3 flex flex-col items-center justify-center">   
-                    <div className="absolute left-[55%] top-[7rem] sm:top-[9rem] lg:top-[10rem] xl:top-[14rem]">
+                <div className=" w-1/2 md:w-1/3 flex flex-col items-center justify-center">   
+                    <div className="absolute left-[55%] top-[7rem] hidden md:block sm:top-[9rem] lg:top-[10rem] xl:top-[14rem]">
                         <img src={V6} alt="Vector" className="h-[1rem] sm:h-[1.5rem] lg:h-[2rem] xl:h-[3rem]"/>
                     </div>
                     <div>
@@ -39,12 +39,12 @@ const WorkHero = () => {
                         </div>
                     </div>
                     
-                    <div className="absolute left-[39%] top-[9.3rem] sm:top-[12.5rem] lg:top-[14.5rem] xl:top-[19.5rem] z-0">
+                    <div className="absolute left-[39%] top-[9.3rem] hidden md:block sm:top-[12.5rem] lg:top-[14.5rem] xl:top-[19.5rem] z-0">
                         <img src={V5} alt="Vector" className="h-[1.5rem] sm:h-[2rem] xl:h-[3rem]"/>
                     </div>
                 </div>
-                <div className="w-1/3 relative">
-                    <div className="absolute z-10 -left-4 sm:-left-10 xl:-left-16 top-[7rem] sm:top-[9rem] xl:top-[12rem]">
+                <div className="w-1/2 md:w-1/3 relative">
+                    <div className="absolute z-10 -left-4 hidden md:block sm:-left-10 xl:-left-16 top-[7rem] sm:top-[9rem] xl:top-[12rem]">
                         <img src={V4} alt="Vector" className="h-[3rem] sm:h-[5rem] lg:h-[8rem] xl:h-[11rem]"/>
                     </div>
                     <div className="absolute z-20 right-6 sm:right-10 top-[6.5rem] sm:top-28 xl:top-32">
@@ -58,10 +58,10 @@ const WorkHero = () => {
                     </div>
                 </div>
             </div>
-            <div className="relative px-[1.5rem] -top-16 sm:-top-20 xl:px-[3rem] xl:-top-28">
+            <div className="relative px-[1rem] -top-28 sm:-top-20 xl:px-[3rem] xl:-top-28">
                 <img
                 src={GotImage}
-                alt="">   
+                alt="" className=" h-24 sm:h-28 md:h-full">   
                 </img>
             </div>
         </div>
