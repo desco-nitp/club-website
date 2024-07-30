bf660d7da1c222dd7a0e76cc4e10a8ebc5e04b2c
Mitali Dixit <mitali.dixit04@gmail.com>
2024-07-30 19:17:52 +0530

    fetching blogs data from firebase and rendering

--- frontend/src/components/blogs/part1.jsx
+++ frontend/src/components/blogs/part1.jsx
@@ -1,20 +1,18 @@
-import React from 'react'
-import Feature from './feature/featuredpost'
-import Categories from './feature/categories'
+import React from 'react';
+import Feature from './feature/featuredpost';
+import Categories from './feature/categories';
 
-function Part1() {
+function Part1({ featuredBlog }) {
   return (
-    <div className='bg-[#FEB5B5] mt-10 mb-20 lg:grid  md:grid-cols-2  '>
-        <div className='flex justify-center' >
-            <Feature />
-        </div>
-        <div className='flex justify-center items-center  h-[110vh]'>
-            <Categories />
-        </div>
-    
+    <div className='bg-[#FEB5B5] mt-10 mb-20 lg:grid  md:grid-cols-2'>
+      <div className='flex justify-center'>
+        <Feature blog={featuredBlog} />
+      </div>
+      <div className='flex justify-center items-center  h-[110vh]'>
+        <Categories />
+      </div>
     </div>
-    
-  )
+  );
 }
 
-export default Part1
+export default Part1;
