04961722c01671f5e44375f629d998d49ce2ea80
maithilee24 <130568585+maithilee24@users.noreply.github.com>
2024-08-07 14:13:33 +0530

    Responsiveness of works page (#54)
    * Workpage
    
    * changes done
    
    * WorksSection
    
    * updated
    
    * Responsiveness of works page
    
    * Changes done

--- frontend/src/components/works/works_herosection/works_hero.jsx
+++ frontend/src/components/works/works_herosection/works_hero.jsx
@@ -9,60 +9,77 @@ import V4 from "./V4.webp";
 import V5 from "./V5.webp";
 import V6 from "./V6.webp";
 import GotImage from "./GotaProjectImage.webp";
-import "./style.css"
+import "./style.css";
 
 const WorkHero = () => {
     return (
-        <div className="flex flex-col">
-            <div className="flex flex-row relative -top-[1.5rem] ">
-                <div className="w-1/3 relative">
-                    <div className="absolute left-4 sm:left-8 lg:left-10 top-[5.5rem] sm:top-[6.5rem] xl:top-[8rem] z-20">
-                        <img src={Star} alt="Star" className="h-[2rem] sm:h-[3.5rem] lg:h-[4rem] xl:h-[6rem]" />
+        <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center w-full relative -top-[3rem] md:-top-[1.5rem]">
+                {/* Mobile Layout */}
+                <div className="flex flex-col items-center w-full md:hidden relative">
+                    <div className="w-1/2 absolute right-0 rotate-180">
+                        <img src={Cross} alt="Cross" className="inset-0 z-10 h-[14rem] sm:h-[18rem]" />
                     </div>
-                    <div className="absolute left-4 sm:left-8 top-[8.5rem] sm:top-[11rem] lg:left-10 lg:top-[13rem] xl:top-[18rem] z-20">
-                        <img src={V1} alt="Vector" className="h-[4rem] sm:h-[6rem] lg:h-[8rem] xl:h-[11rem]" />
-                    </div>
-                    <div className="relative z-0 ">
-                        <img src={Cross} alt="Cross" className=" h-[18rem] sm:h-[24rem] lg:h-[28rem] xl:h-[38rem]" />
-                    </div>
-                    <div className="absolute -right-3 sm:-right-9 xl:-right-16 top-[7rem] sm:top-[9rem] xl:top-[12rem] z-20">
-                        <img src={V4} alt="Vector" className="h-[3rem] sm:h-[5rem] lg:h-[8rem] xl:h-[11rem]"/>
-                    </div>
-                </div>
-                <div className="w-1/3 flex flex-col items-center justify-center">   
-                    <div className="absolute left-[55%] top-[7rem] sm:top-[9rem] lg:top-[10rem] xl:top-[14rem]">
-                        <img src={V6} alt="Vector" className="h-[1rem] sm:h-[1.5rem] lg:h-[2rem] xl:h-[3rem]"/>
-                    </div>
-                    <div>
-                        <div className="syne-fon text-[1.5rem] sm:text-[2rem] md:text-[2.5rem] lg:text-[3.5rem] xl:text-[4.5rem] text-white font-extrabold  text-shadow  relative z-20">
-                            Our Works
+                    <div className="flex flex-col items-center relative z-20 mt-24">
+                        <div className="flex items-center">
+                            <img src={V1} alt="Vector" className="h-[3.5rem] sm:h-[6rem] rotate-90 mr-4 sm:mr-8" />
+                            <div className="text-center">
+                                <div className="syne-fon text-[2rem] sm:text-[3rem] text-white font-extrabold text-shadow">
+                                Our Works
+                                </div>
+                            </div>
+                            <img src={Star} alt="Star" className="h-[2.5rem] sm:h-[4rem] ml-2 sm:ml-6" />
                         </div>
                     </div>
-                    
-                    <div className="absolute left-[39%] top-[9.3rem] sm:top-[12.5rem] lg:top-[14.5rem] xl:top-[19.5rem] z-0">
-                        <img src={V5} alt="Vector" className="h-[1.5rem] sm:h-[2rem] xl:h-[3rem]"/>
-                    </div>
                 </div>
-                <div className="w-1/3 relative">
-                    <div className="absolute z-10 -left-4 sm:-left-10 xl:-left-16 top-[7rem] sm:top-[9rem] xl:top-[12rem]">
-                        <img src={V4} alt="Vector" className="h-[3rem] sm:h-[5rem] lg:h-[8rem] xl:h-[11rem]"/>
+
+                {/* Desktop Layout */}
+                <div className="hidden md:flex flex-row relative w-full">
+                    <div className="w-1/3 relative">
+                        <div className="absolute md:left-8 lg:left-10 md:top-[6.5rem] xl:top-[8rem] z-20">
+                            <img src={Star} alt="Star" className="md:h-[3.5rem] lg:h-[4rem] xl:h-[6rem]" />
+                        </div>
+                        <div className="absolute md:left-8 md:top-[11rem] lg:left-10 lg:top-[13rem] xl:top-[18rem] z-20">
+                            <img src={V1} alt="Vector" className="md:h-[6rem] lg:h-[7rem] xl:h-[9rem]" />
+                        </div>
+                        <div className="relative z-0">
+                            <img src={Cross} alt="Cross" className="w-full md:h-[24rem] lg:h-[26rem] xl:h-[35rem]" />
+                        </div>
+                        <div className="absolute hidden md:block md:-right-9 xl:-right-16 md:top-[7rem] xl:top-[10rem] z-20">
+                            <img src={V4} alt="Vector" className="md:h-[7rem] lg:h-[8rem] xl:h-[11rem]" />
+                        </div>
                     </div>
-                    <div className="absolute z-20 right-6 sm:right-10 top-[6.5rem] sm:top-28 xl:top-32">
-                        <img src={V3} alt="Vector" className="h-[2rem] sm:h-[3.5rem] lg:h-[5rem] xl:h-[7rem]"/>
+                    <div className="w-full md:w-1/3 flex flex-col items-center justify-center">
+                        <div className="absolute left-[55%] hidden md:block md:top-[8rem] xl:top-[12rem]">
+                            <img src={V6} alt="Vector" className="md:h-[2rem] lg:h-[2.5rem] xl:h-[3rem]" />
+                        </div>
+                        <div>
+                            <div className="syne-fon text-[1.5rem] sm:text-6xl md:text-[2.5rem] lg:text-[3.65rem] xl:text-[4.5rem] text-white font-extrabold text-shadow relative z-20">
+                                Our Works
+                            </div>
+                        </div>
+                        <div className="absolute left-[39%] hidden md:block md:top-[12rem] lg:top-[13.5rem] xl:top-[18rem] z-0">
+                            <img src={V5} alt="Vector" className="md:h-[2.5rem] lg:h-[3rem]" />
+                        </div>
                     </div>
-                    <div className="absolute z-20 right-2 sm:right-4 lg:right-2 xl:right-0 top-40 sm:top-52 lg:top-56 xl:top-72">
-                        <img src={V2} alt="Vector" className="h-[4rem] sm:h-[6rem] lg:h-[8rem] xl:h-[10rem]"/>
-                    </div>
-                    <div className="relative z-0  lg:-right-14 xl:-right-5 lg:top-4 xl:top-2 ">
-                        <img src={Circle} alt="Circle" className="h-[18rem] sm:h-[24rem] mt-10 md:mt-0 lg:h-[26rem] xl:h-[35rem]"/>
+                    <div className="hidden md:block w-1/3 relative">
+                        <div className="absolute z-10 md:-left-10 xl:-left-16 md:top-[7rem] xl:top-[10rem]">
+                            <img src={V4} alt="Vector" className="md:h-[7rem] lg:h-[8rem] xl:h-[11rem]" />
+                        </div>
+                        <div className="absolute z-20 right-0 sm:right-10 top-[6.5rem] sm:top-28 xl:top-32">
+                            <img src={V3} alt="Vector" className="h-[2rem] sm:h-[3.5rem] lg:h-[4rem] xl:h-[7rem]" />
+                        </div>
+                        <div className="absolute z-20 right-0 sm:right-4 lg:right-2 xl:right-0 top-40 sm:top-52 lg:top-56 xl:top-72">
+                            <img src={V2} alt="Vector" className="h-[4rem] sm:h-[6rem] lg:h-[7rem] xl:h-[9rem]" />
+                        </div>
+                        <div className="relative z-0 w-full">
+                            <img src={Circle} alt="Circle" className="w-full h-[18rem] sm:h-[24rem] lg:h-[26rem] xl:h-[35rem]" />
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="relative px-[1.5rem] -top-16 sm:-top-20 xl:px-[3rem] xl:-top-28">
-                <img
-                src={GotImage}
-                alt="">   
-                </img>
+            <div className="hidden md:block relative md:px-[2rem] md:-top-20 xl:px-[3rem] xl:-top-28 w-full">
+                <img src={GotImage} alt="" className="w-full h-auto" />
             </div>
         </div>
     );
--- frontend/src/components/works/works_section/banner.jsx
+++ frontend/src/components/works/works_section/banner.jsx
@@ -6,7 +6,7 @@ import Image from "./image";
 
 function Banner({ title, detail, images }){
     return (
-        <div className="bg-white relative p-2 sm:p-4 mb-20 mx-10 md:mb-32 md:mx-16 h-48 sm:h-60 md:h-72 lg:h-80 xl:h-96 rounded-2xl border-[3px] border-black border-b-[6px] border-r-[6px]">
+        <div className="bg-white relative p-2 sm:p-4 mb-20 mx-6 md:mb-32 sm:mx-10 lg:mx-16 h-[10rem] sm:h-52 md:h-64 lg:h-[19rem] xl:h-[22rem] rounded-2xl border-[3px] border-black border-b-[6px] border-r-[6px]">
             <div className=" bg-[#282828] text-white p-2 absolute -top-6 left-8 sm:-top-8 lg:-top-10 sm:left-12 h-9 w-32 sm:h-12 sm:w-44 lg:h-16 lg:w-52 -rotate-6 rounded-2xl flex items-center justify-center">
                 <div className="bowlby-one text-sm sm:text-lg lg:text-2xl text-bold">{title}</div>
             </div>
@@ -18,7 +18,7 @@ function Banner({ title, detail, images }){
                     <img src={Arrow} alt="Arrow" className=" w-8 sm:w-14"/>
                 </div>
             </div>
-            <img src={V1} alt="V1" className="absolute w-14 sm:w-16 md:w-20 lg:w-28 top-0 -right-2" />
+            <img src={V1} alt="V1" className="absolute w-[2.8rem] sm:w-14 md:w-16 lg:w-20 xl:w-24 top-0 -right-2" />
         </div>
     );
 }
